42ed347cc53a56b8198fa45224a7b81e59432876
pftrobot <106806706+pftrobot@users.noreply.github.com>
2024-03-21 21:09:23 +0900

    fix: export mode

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  distDir: "build",
+  output: "export",
   trailingSlash: true,
   compiler: {
     emotion: true
